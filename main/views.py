5621de685dd2acca8788220713312ef5b057004c
jaehyuen <violet6520@naver.com>
2022-11-01 20:28:54 +0900

    [2022/11/01 이재현] url 반영

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def login(request):
+    return render(request, 'main/login.html')
+
+def register(request):
+    return render(request, 'main/register.html')
