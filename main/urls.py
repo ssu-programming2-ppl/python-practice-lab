bd11cfc6c4eaa46421e534318130a0163826c70d
jaehyuen <violet6520@naver.com>
2022-11-05 22:43:10 +0900

    [2022/11/05 이재현] 화면 및 라이브러리 추가
    1. 문제 생성 및 문제 풀이 화면 추가
    2. MDB라이브러리 추가

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('list', views.list, name='list'),
+    path('create', views.create, name='create'),
+    path('test', views.test, name='test'),
+    path('question', views.question, name='question')
 ]
--- main/views.py
+++ main/views.py
@@ -12,3 +12,33 @@ def register(request):
 
 def list(request):
     return render(request, 'main/list.html')
+
+def create(request):
+    return render(request, 'main/create.html')
+
+def test(request):
+    return render(request, 'main/test.html')
+
+def question(request):
+    seq = request.GET.get('questionSeq', str(1))
+    data = getQuestionInfo(seq)
+    print(data)
+    
+    return render(request, 'main/question.html', data)
+
+def getQuestionInfo(seq):
+
+    data = {
+        "question_seq": "test data1 " + seq,
+        "question_title": "test data2 " + seq,
+        "question_level": "test data3 " + seq,
+        "question_rating": "test data4 " + seq,
+        "question_lang": "test data5 " + seq,
+        "question_text": "test data6 " + seq,
+        "question_code": "test data7 " + seq,
+        "created_at": "test data8 " + seq,
+        "modified_at": "test data9 " + seq,
+        "user_id": "test data10 " + seq
+    }
+
+    return data
