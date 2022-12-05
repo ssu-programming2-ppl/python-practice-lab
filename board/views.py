d5707766ec8cca84cfed35350deba05325338663
jaehyuen <violet6520@naver.com>
2022-12-05 20:56:46 +0900

    [2022/12/05 이재현] 디자인 수정 2

--- board/views.py
+++ board/views.py
@@ -14,15 +14,12 @@ def board_list(request):
     page = request.GET.get("page", "1")
     limit = request.GET.get("limit", "2")
 
-    blog_list = Board.objects.filter(board_division="blog").order_by("-board_seq")
-    youtube_list = Board.objects.filter(board_division="youtube").order_by("-board_seq")
+    board_list = Board.objects.all().order_by("-board_seq")
 
-    blog_paginator = Paginator(blog_list, limit)
-    youtube_paginator = Paginator(youtube_list, limit)
+    board_paginator = Paginator(board_list, limit)
 
     data = {
-        "blog_list": blog_paginator.get_page(page),
-        "youtube_list": youtube_paginator.get_page(page),
+        "board_list": board_paginator.get_page(page),
     }
 
     # TODO(김금주) 게시판 목록 조회 개발 필요 ** 페이징 처리 필요
