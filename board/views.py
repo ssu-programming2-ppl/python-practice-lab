06bd2144c3c325a91698d2ac8afac1fd709970ae
heejin-pro <hjin.pro@gmail.com>
2022-12-06 21:21:01 +0900

    [2022/12/06 박희진] 게시판 등록 로직 구현

--- board/views.py
+++ board/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import json
+from core import utils
+from board.models import Board
+from main.models import User
+from django.db import transaction
+
 # Create your views here.
 
 
@@ -13,14 +19,18 @@ def board_list(request):
 
 
 @login_required(login_url='/login')
+@transaction.atomic()
 def board_create(request):
-
     if request.method == "GET":
         return render(request, "board_create.html")
     else:
-
         body = json.loads(request.body.decode('utf-8'))
+        board = Board()
+        board.board_title = body['board_title']
+        board.board_view_count = 0
+        board.board_link = body['board_link']
+        board.board_division = body['board_division']
+        board.user_id = request.user
+        board.save()
 
-        # TODO(김금주) 문제 생성 로직 필요
-        # 1. body 변수에서 게시판 정보를 추출후 게시판 테이블에 저장
-        return render(request, "board_create.html")
+        return utils.create_ressult(None, "저장 성공", True)
